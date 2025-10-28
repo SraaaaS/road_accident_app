19ca0a045ebd4cee490a1954bb2fe940d4e32de8
SraaaaS <saramerabli@gmail.com>
2025-10-28 15:43:44 +0100

    Adjust welcome message font size and format description
    Updated the font size in the welcome message and improved the formatting of the application description.

--- frontend/section/home.py
+++ frontend/section/home.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 def app():
-    st.markdown("<p style='color:#348781; font-size:36px; font-weight:bold'>Bienvenue sur la page d'accueil de l'application Road Accident Analysis", unsafe_allow_html=True)
+    st.markdown("<p style='color:#348781; font-size:34px; font-weight:bold'>Bienvenue sur la page d'accueil de l'application Road Accident Analysis", unsafe_allow_html=True)
     st.write("""Cette application vise à analyser les données des accidents de la route pour identifier les **tendances** et les **facteurs contributifs**.
-                  Vous pouvez naviguer à travers les différentes sections de l'application pour **explorer les données**, **visualiser les analyses** et obtenir des **insights** précieux.
-             Utilisez le **menu de navigation** sur le côté pour accéder aux différentes fonctionnalités.""")
+            \nVous pouvez naviguer à travers les différentes sections de l'application pour **explorer les données**, **visualiser les analyses** et obtenir des **insights** précieux.
+             \nUtilisez le **menu de navigation** sur le côté pour accéder aux différentes fonctionnalités.""")
     st.image("frontend/components/home_pic.png", use_column_width=True)
     
