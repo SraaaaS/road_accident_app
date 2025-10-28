93e33270511efe3a1d188bdcd3f743252e25e0db
SraaaaS <saramerabli@gmail.com>
2025-10-28 15:36:48 +0100

    Update homepage styling and text size

--- frontend/section/home.py
+++ frontend/section/home.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
 def app():
-    st.markdown("<p style='color:#348781; font-size:42px; font-weight:bold'>Bienvenue sur la page d'accueil de l'application Road Accident Analysis", unsafe_allow_html=True)
-    st.image("frontend/components/home_pic.png", use_column_width=True)
+    st.markdown("<p style='color:#348781; font-size:36px; font-weight:bold'>Bienvenue sur la page d'accueil de l'application Road Accident Analysis", unsafe_allow_html=True)
     st.write("""Cette application vise à analyser les données des accidents de la route pour identifier les **tendances** et les **facteurs contributifs**.
-              Vous pouvez naviguer à travers les différentes sections de l'application pour **explorer les données**, **visualiser les analyses** et obtenir des **insights** précieux.
+                  Vous pouvez naviguer à travers les différentes sections de l'application pour **explorer les données**, **visualiser les analyses** et obtenir des **insights** précieux.
              Utilisez le **menu de navigation** sur le côté pour accéder aux différentes fonctionnalités.""")
-    
+    st.image("frontend/components/home_pic.png", use_column_width=True)
     
