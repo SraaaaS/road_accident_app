a571a7c6db10d7508cd970f4530a3aedbcb198a2
SraaaaS <saramerabli@gmail.com>
2025-10-28 16:36:36 +0100

    Update welcome message and captions in home.py

--- frontend/section/home.py
+++ frontend/section/home.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
 def app():
-    st.markdown("<p style='color:#348781; font-size:34px; font-weight:bold'>Bienvenue sur la page d'accueil de l'application Road Accident Analysis", unsafe_allow_html=True)
+    st.markdown("<p style='color:#348781; font-size:34px; font-weight:bold'>Bienvenue sur la page d'accueil de l'APAR* !", unsafe_allow_html=True)
     st.write("""Cette application vise à analyser les données des accidents de la route pour identifier les **tendances** et les **facteurs contributifs**.
             \nVous pouvez naviguer à travers les différentes sections de l'application pour **explorer les données**, **visualiser les analyses** et obtenir des **insights** précieux.
              \nUtilisez le **menu de navigation** sur le côté pour accéder aux différentes fonctionnalités.""")
     st.image("frontend/components/home_pic.png", use_column_width=True)
-    st.caption("Image illustrative de la competition <span style='font-style:italic'>Playground Series - Season 5, Episode 10 sur Kaggle<\span>", unsafe_allow_html=True )
+    st.caption("Image illustrative de la competition <span style='font-style:italic'>Playground Series - Season 5, Episode 10 sur Kaggle</span>", unsafe_allow_html=True )
+    st.caption("\nAPAR = Application de Prévention contre les Accidents Routiers")
     
